617d47f7923a699d3811e75280b113c7ac5a0ac1
mg143pavankumar <pavansharma.mg143@gmail.com>
2021-09-18 12:12:18 +0530

    done form validation

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import "../styles/globals.css";
 import { StoreProvider } from "../utils/Store";
+import { SnackbarProvider } from "notistack";
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
@@ -12,9 +13,11 @@ function MyApp({ Component, pageProps }) {
   }, []);
 
   return (
-    <StoreProvider>
-      <Component {...pageProps} />{" "}
-    </StoreProvider>
+    <SnackbarProvider anchorOrigin={{ vertical: "top", horizontal: "center" }}>
+      <StoreProvider>
+        <Component {...pageProps} />{" "}
+      </StoreProvider>
+    </SnackbarProvider>
   );
 }
 
--- pages/api/users/login.js
+++ pages/api/users/login.js
@@ -1,27 +1,27 @@
 import nc from "next-connect";
 import User from "../../../models/User";
 import db from "../../../utils/db";
-import bcrypt from 'bcryptjs';
-import {signToken} from '../../../utils/auth';
+import bcrypt from "bcryptjs";
+import { signToken } from "../../../utils/auth";
 
 const handler = nc();
 
-handler.post( async (req, res) => {
+handler.post(async (req, res) => {
   await db.connect();
-  const user = await User.findOne({email: req.body.email});
+  const user = await User.findOne({ email: req.body.email });
   await db.disconnect();
 
-  if(user && bcrypt.compareSync(req.body.password, user.password)){
+  if (user && bcrypt.compareSync(req.body.password, user.password)) {
     const token = signToken(user);
     res.send({
       token,
       _id: user._id,
-       name: user.name,
-       email: user.email,
-       isAdmin: user.isAdmin
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
     });
-  }else{
-    res.status(401).send({message: 'Invalid user or password'})
+  } else {
+    res.status(401).send({ message: "Invalid email or password" });
   }
 });
 
